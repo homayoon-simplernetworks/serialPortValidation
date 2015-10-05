c00c3d369e600376d0d34872d0455b1ffde008af
homayoon <homayoon@simplernetworks.com>
2015-10-05 17:44:17 -0400

    almost working, login is good

--- serialPortValidation/serialPortValidation.py
+++ serialPortValidation/serialPortValidation.py
@@ -1,15 +1,21 @@
-#Simplernetworks
+﻿#Simplernetworks
 
 import time
 import serial
+import colorama
+import sys
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(port='COM3',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS
-)
+try:
+    ser = serial.Serial(port='COM3',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout = 1
+    )
+except  :
+    print (port + '  is not ready please check the port and try again ')
 
 if ser.isOpen():
     ser.close()
@@ -17,23 +23,75 @@ ser.open()
 ser.isOpen()
 
 print ('Enter your commands below.\r\nInsert "exit" to leave the application.')
+#send inter to serial port to start communication with ez-edge
+ser.write(b'''\r''')
 
-input1='dsa'
-while 1 :
+
+# this part of program will remove the ESC ansi codes from strings 
+from pyparsing import *
+ESC = Literal('\x1b')
+integer = Word(nums)
+escapeSeq = Combine(ESC + '[' + Optional(delimitedList(integer,';')) + 
+                oneOf(list(alphas)))
+nonAnsiString = lambda s : Suppress(escapeSeq).transformString(s)
+
+
+def serialLogin():
+    global isLogin
+    isLogin = False
+    while not isLogin:
+        response = ser.readlines()
+        time.sleep(0.5)       
+        for line in response:
+            li = line.decode()
+            unColorString = nonAnsiString(li)
+            print(unColorString )
+            #check if asking for user:
+            sUser = 'admin'
+            sPass = 'ez-edge#1\r\r'
+            
+            if li.find('User:')>= 0: 
+                ser.write(sUser.encode('ascii'))
+            elif li.find('Password:')>= 0: 
+                ser.write(sPass.encode('ascii'))
+                ser.readline()
+                ser.write(b'\r')
+            elif li.find('Press any key')>= 0: 
+                ser.write(b'\r')
+            elif li.find('Enter number ')>=0 : isLogin=True
+            #else:
+                #time.sleep(0.5) 
+                #ser.write(b'''\r''')
+        if not isLogin: ser.write(b'''\r''')
+
     
-        # send the character to the device
-        # (note that I happed a \r\n carriage return and line feed to the characters - this is requested by my device)
-        input1 = '\r\n'
-        ser.write(input1.encode('utf-8'))
-        out = ''
-        # let's wait one second before reading output (let's give device time to answer)
-        time.sleep(1)
-        #time.sleep(5)
-        while True:
-           response = ser.readline()
+    #in this stage we expect serial port is logged in successfully 
+    input1 = input('>> ') 
+    ser.write(input1.encode('ascii' , 'replace'))
+    #time.sleep(1)
+    rr = ser.read()
+    time.sleep(0.5)
+    ser.write(b'\r')
+     
+
+
+
+serialLogin()
+
+while 1 :
+           response = ser.readlines()
            
-           print("read data: " , str(response) , response.decode('utf-8','ignore' ) )
-           time.sleep(1)
-           ser.write(input1.encode('utf-8'))
+           for li in response:
+               unColorString = nonAnsiString(li.decode())
+               print(unColorString )
+           
+           input1 = input('>> ') 
+           ser.write(input1.encode('ascii' , 'replace'))
+           
+           rr = ser.read()
+           time.sleep(0.5)
+           ser.write(b'\r')
+           
+           
        
 ser.close()
