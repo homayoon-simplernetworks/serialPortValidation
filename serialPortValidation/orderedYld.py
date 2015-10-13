ae58d1e4ae3a9e0bc7f4e725dc25420520242e8a
homayoon <homayoon@simplernetworks.com>
2015-10-13 14:07:26 -0400

    start to write code for testing the options

---
+++ serialPortValidation/orderedYld.py
@@ -0,0 +1,25 @@
+﻿import yaml
+from collections import OrderedDict
+import ordered
+from ordered import *
+
+class orderedYld(object):
+    """description of class"""
+    def __init__ (self):
+                data = {}
+
+    def orderedYaml_loader(filepath):
+                """Loads a yaml file"""
+                oorr = ordered
+                with open(filepath, "r") as file_descriptor:
+                        data = oorr.ordered_load (file_descriptor, yaml.SafeLoader)
+                        #data = ordered_load(file_descriptor, yaml.SafeLoader)
+                return data
+
+    def orderedYmal_dump(filepath, data):
+                """Dump data to a yaml file"""
+                oorw = ordered
+                with open(filepath, "w") as file_desxriptor:
+                        #yaml.dump(data, file_desxriptor , default_flow_style=False)
+                        oorw.ordered_dump(data, file_desxriptor ,  Dumper=yaml.SafeDumper , default_flow_style=False)
+    
